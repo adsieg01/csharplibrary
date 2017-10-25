d9bdb587d9ac44a072c760f200e9fbc72dca392d
adsieg01 <adsieg01@gmail.com>
2017-10-25 11:14:57 -0400

    Initial Commit

---
+++ 0.02_BasicTypes/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._02_BasicTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 15;
+            int number2 = 5;
+            int number3 = 10;
+
+            byte myByte = 255; // range: 0 - 255
+
+            float myFloat = 3.14f;
+
+            double myDouble = 2.54; 
+
+            decimal myDecimal = 3.14159M;
+
+            char myLetter = 'A';
+
+            string myName = "Jay";
+
+            bool myTruth = true;
+
+            Console.WriteLine(myName);
+          
+            // Declaration
+            // <type> <variableName> = <optioinal initializer>;
+            int age;
+            string country;
+
+            //Initialization
+            age = 41;
+            Console.WriteLine(age);
+         
+            age = 42;
+            
+
+        }
+    }
+}
---
+++ 0.02_BasicTypesChallenges/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._02_BasicTypesChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number1 = 9;
+            int number2 = 72;
+
+            string myName = "Jay";
+            string myRealName = "Andrew";
+
+            bool myTruth = true;
+            bool NotMyTruth = false;
+
+            decimal myDecimal = 2.5543M;
+            decimal givenChange = 9.534345M;
+
+            string fullName = "Andrew" + " Sieg";
+            Console.WriteLine(fullName);
+            Console.ReadLine();
+
+            string yearBorn = "2017";
+            int.TryParse(yearBorn);
+
+
+            //Gold
+            string birthYear = "1990";
+            int year = Convert.ToInt32(birthYear);
+            Console.WriteLine(year);
+            Console.ReadLine();
+
+
+        }
+    }
+}
