5c0382705e816a3146db7db2a4eb33b4e6ab8e4f
adsieg01 <adsieg01@gmail.com>
2017-10-25 14:10:01 -0400

    Bank Challenge

---
+++ 0.04_BankAccount/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._04_BankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Bank Account Balance");
+            Console.ReadLine();
+
+            string balance = Console.ReadLine();
+           int realNumber = Convert.ToInt32(balance);
+          
+
+            if (realNumber > 10000)
+            {
+                Console.WriteLine("You're rich");
+                Console.ReadLine();
+            }
+            else if (realNumber < 10000)
+            {
+                Console.WriteLine("You're poor");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Invalid Entry");
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 0.04_Coditionals/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._04_Coditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter on a scale of 1-5 how you are feeling today.");
+            Console.Write("Enter here: " );
+            string feelingNumber = Console.ReadLine();
+
+            if (feelingNumber == "1")
+            {
+                Console.WriteLine("That sucks");
+            }
+            else if (feelingNumber == "2")
+            {
+                Console.WriteLine("could be better");
+            }
+            else if (feelingNumber == "3")
+            {
+                Console.WriteLine("could be worse");
+            }
+            else if (feelingNumber == "4")
+            {
+                Console.WriteLine("Not bad!");
+            }
+            else if (feelingNumber == "5")
+            {
+                Console.WriteLine("Let's Party!");
+            }
+            else
+            {
+                Console.Write("Sorry I don't understand.");
+            }
+
+
+
+        }
+    }
+}
