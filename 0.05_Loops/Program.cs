b0a8dd053ca9e87f8766bda923b0d825eadd5429
adsieg01 <adsieg01@gmail.com>
2017-10-25 16:11:10 -0400

    fizz Buzz

---
+++ 0.05_Loops/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._05_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+
+           
+
+            for (int i = 0; i < 11; i++)
+            {
+                if (i == 10)
+                {
+                    Console.WriteLine(i);
+                    Console.WriteLine("Blast off!");
+                }
+            }
+        }
+    }
+}
---
+++ 0.06_LoopChallenge/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._06_LoopChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            while (n < 101)
+            {
+                if(n % 15 == 0)
+                    {
+                       Console.WriteLine("FizzBuzz");
+                    }
+                else if (n % 3 == 0) 
+                {
+                    Console.WriteLine("Fizz");
+                }
+               else if (n % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(n);
+                }
+                 n+=1;
+            }   }
+    }
+}
