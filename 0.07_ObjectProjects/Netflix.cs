1ac33de9a2c787bed3b6d87cd1cdf6da3a369af6
adsieg01 <adsieg01@gmail.com>
2017-10-26 10:55:22 -0400

    Objects with challenge

---
+++ 0.07_ObjectProjects/Netflix.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._07_ObjectProjects
+{
+    class Netflix
+    {
+        //Properties
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public double Rating { get; set; }
+
+        //Methods
+        public void GetSuggestion()
+        {
+          
+            if (this.Rating > 3)
+            {
+                Console.WriteLine("This is a pretty good show.");
+            }
+            else
+            {
+                Console.WriteLine("This show is not so good.");
+            }
+                
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ 0.07_ObjectProjects/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._07_ObjectProjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            // Object Instantiation
+            Netflix starTrek = new Netflix();
+            Netflix familyGuy = new Netflix();
+            Netflix narcos = new Netflix();
+
+            // Set Object Values
+            starTrek.Name = "Star Trek Enterprise";
+            starTrek.Genre = "Scifi";
+            starTrek.Rating = 3;
+
+            familyGuy.Name = "Family Guy";
+            familyGuy.Genre = "Comedy";
+            familyGuy.Rating = 4;
+
+            narcos.Name = "Narcos";
+            narcos.Genre = "Drama";
+            narcos.Rating = 5;
+
+
+            // Calling the methods on the object 
+
+            //starTrek.GetSuggestion();
+            //familyGuy.GetSuggestion();
+            //narcos.GetSuggestion();
+
+            //Creating a list of Objects
+            List<Netflix> favoriteShows = new List<Netflix>();
+
+            favoriteShows.Add(starTrek);
+            favoriteShows.Add(familyGuy);
+            favoriteShows.Add(narcos);
+
+            foreach (Netflix show in favoriteShows)
+            {
+                Console.WriteLine(show.Name);
+                Console.WriteLine(show.Genre);
+                Console.WriteLine(show.Rating);
+            }
+
+
+
+            
+            
+
+
+
+
+        }
+
+        }
+}
