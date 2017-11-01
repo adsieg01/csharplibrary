0298448c1eb703ec1e30e574b53be09fe41ab936
adsieg01 <adsieg01@gmail.com>
2017-11-01 14:43:04 -0400

    overrides

--- 0.14_Override/Circle.cs
+++ 0.14_Override/Circle.cs
@@ -8,13 +8,16 @@ namespace _0._14_Override
 {
     class Circle : Shape
     {
+        //Properties
         public int RadiusSize { get; set; }
 
+        // Constructor
         public Circle (int radius)
         {
             this.RadiusSize = radius; 
         }
 
+        //Methods
         public override double Area()
         {
             return Math.Pow(RadiusSize, 2)*Math.PI;
