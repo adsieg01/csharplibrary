1263246a1790412f3b2326a4efa3b6a48c07c144
adsieg01 <adsieg01@gmail.com>
2017-11-02 09:38:32 -0400

    Quiz One

--- 0.16_QuizOne/Program.cs
+++ 0.16_QuizOne/Program.cs
@@ -10,6 +10,25 @@ namespace _0._16_QuizOne
     {
         static void Main(string[] args)
         {
+            Triangle newTriangle = new Triangle(8,4);
+
+            Console.WriteLine($"Area = {newTriangle.Area()}");
+
+            Triangle newTriangle2 = new Triangle(3, 1);
+
+            Console.WriteLine($"Area = {newTriangle2.Area()}");
+
+            Triangle newTriangle3 = new Triangle(6, 6);
+
+            Console.WriteLine($"Area = {newTriangle3.Area()}");
+
+            Triangle newTriangle4 = new Triangle(3, 4);
+
+            Console.WriteLine($"Area = {newTriangle4.Area()}");
+
+            Triangle newTriangle5 = new Triangle(10, 40);
+
+            Console.WriteLine($"Area = {newTriangle5.Area()}");
         }
     }
 }
---
+++ 0.16_QuizOne/Triangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._16_QuizOne
+{
+    class Triangle : Shape
+    {
+        //Properties
+       
+        public int Base { get; set; }
+        public int Height { get; set; }
+
+        //Constructor
+        public Triangle(int tbase, int theight)
+        {
+            this.Base = tbase;
+            this.Height = theight;
+
+        }
+
+        //Methods
+        public override double Area()
+        {
+            return this.Base*.5*(this.Height);
+        }
+    }
+}
