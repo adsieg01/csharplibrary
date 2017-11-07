752090f27ccd198b80f9f191a41d00cfdc196247
adsieg01 <adsieg01@gmail.com>
2017-11-07 14:17:40 -0500

    finishing for presentation

---
+++ 0.17_Interfaces/ListClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._17_Interfaces
+{
+    class ListClass : IEnum, IList, ICollect
+    {
+        public void AddStuff()
+        {
+            Console.WriteLine("Add stuff to list");
+        }
+
+        public void CheckCompacity()
+        {
+            Console.WriteLine("Check compacity of list");
+        }
+
+        public void CheckCount()
+        {
+            Console.WriteLine("Checking count of list");
+        }
+
+        public void IterateOverCollection()
+        {
+            Console.WriteLine("Iterating over list");
+        }
+
+        public void RemoveStuff()
+        {
+            Console.WriteLine("Removing stuff from list");
+        }
+    }
+}
---
+++ 0.17_Interfaces/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._17_Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayClass exampleArrayClass = new ArrayClass();
+            exampleArrayClass.CheckCompacity();
+
+            ListClass exampleListClass = new ListClass();
+            exampleListClass.CheckCompacity();
+        }
+    }
+}
